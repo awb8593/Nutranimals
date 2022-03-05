2cf2af66c35163dbd3004356a3872132b50350f4
Alex Leute <acleute@gmail.com>
2022-03-05 16:12:29 -0500

    a very basic log.js complete

---
+++ log.js
@@ -0,0 +1,13 @@
+class Log {
+    /**
+     * Constructor for a log message
+     * @param {string} message message
+     * @param {int} happiness value from 0-2, for storing the emoji that is used
+     */
+    constructor(newMessage, newHappiness){
+        this.message = newMessage; 
+        this.happiness = newHappiness;
+    }
+    
+}
+
