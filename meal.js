dbfd1b793628e47f5ae23cb9e71e06bf9ef6447c
awb8593 <awb8593@rit.edu>
2022-03-05 14:16:32 -0500

    updated meal.js
    updated meal.js with constructor and setters (first commit did not actually include any content for some reason)

---
+++ meal.js
@@ -0,0 +1,99 @@
+
+// class for a Meal object
+class Meal {
+    constructor(name, reflection, calories, totalFat, saturatedFat, transFat, sodium, totalCarb, fiber, totalSugar, 
+        protein, potassium, vitaminA, vitaminB, vitaminC, vitaminD, calcium, iron) {
+        this.name = name;
+        this.reflection = reflection;
+        this.calories = calories;
+        this.totalFat = totalFat;
+        this.saturatedFat = saturatedFat;
+        this.transFat = transFat;
+        this.sodium = sodium;
+        this.totalCarb = totalCarb;
+        this.totalSugar = totalSugar;
+        this.fiber = fiber;
+        this.protein = protein;
+        this.potassium = potassium;
+        this.vitaminA = vitaminA;
+        this.vitaminB = vitaminB;
+        this.vitaminC = vitaminC;
+        this.vitaminD = vitaminD;
+        this.calcium = calcium;
+        this.iron = iron;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setReflection(reflection) {
+        this.reflection = reflection;
+    }
+
+    setCalories(calories) {
+        this.calories = calories;
+    }
+
+    setTotalFat(totalFat) {
+        this.totalFat = totalFat;
+    }
+
+    setSaturatedFat(saturatedFat) {
+        this.saturatedFat = saturatedFat;
+    }
+
+    setTransFat(transFat) {
+        this.transFat = transFat;
+    }
+
+    setSodium(sodium) {
+        this.sodium = sodium;
+    }
+
+    setTotalCarb(totalCarb) {
+        this.totalCarb = totalCarb;
+    }
+
+    setTotalSugar(totalSugar) {
+        this.totalSugar = totalSugar;
+    }
+
+    setFiber(fiber) {
+        this.fiber = fiber;
+    }
+
+    setProtein(protein) {
+        this.protein = protein;
+    }
+
+    setPotassium(potassium) {
+        this.potassium = potassium;
+    }
+
+    setVitaminA(vitaminA) {
+        this.vitaminA = vitaminA;
+    }
+
+    setVitaminB(vitaminB) {
+        this.vitaminB = vitaminB;
+    }
+
+    setVitaminC(vitaminC) {
+
+        this.vitaminC = vitaminC;
+    }
+
+    setVitaminD(vitaminD) {
+        this.vitaminD = vitaminD;
+    }
+
+    setCalcium(calcium) {
+        this.calcium = calcium;
+    }
+
+    setIron(iron) {
+        this.iron = iron;
+    }
+
+}
