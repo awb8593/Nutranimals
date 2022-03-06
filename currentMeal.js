ef7ff930077dadae25b83a23051089c349cf2e59
awb8593 <awb8593@rit.edu>
2022-03-06 04:16:05 -0500

    Complete (For Now)
    Sticker book now correctly displays all information for the selected meal; needs to incorporate reflection and nutranimal once they are finished

--- currentMeal.js
+++ currentMeal.js
@@ -6,19 +6,19 @@ const { Meal } = require('./meal');
 
 var mealToDisplay = new Meal("", null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-// function to help read a json file (returns the meal stored in the json file)
+// function to help read a json file
 function jsonReader(filepath, cb) {
-  fs.readFile(filepath, 'utf-8', (err, fileData) => {
-      if (err) {
-          return cb && cb(err);
-      }
-      try {
-          const object = JSON.parse(fileData);
-          return cb && cb(null, object);
-      } catch (err) {
-          return cb && cb(err);
-      }
-  });
+    fs.readFile(filepath, 'utf-8', (err, fileData) => {
+        if (err) {
+            return cb && cb(err);
+        }
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch (err) {
+            return cb && cb(err);
+        }
+    });
 }
 
 // read the meal to display from the JSON file
@@ -28,10 +28,24 @@ jsonReader('./meals.json', (err, data) => {
     } else {
         mealToDisplay = data;
         console.log("Meal: ", mealToDisplay);
-        document.getElementById('window').innerHTML = mealToDisplay.name;
+        document.title = mealToDisplay.name;
         document.getElementById('header').innerHTML = mealToDisplay.name;
-        document.getElementById('calories').innerHTML = "Calories: ", mealToDisplay.calories;
+        // update rest of the values
+        document.getElementById('calories').innerHTML = mealToDisplay.calories;
+        document.getElementById('totalFat').innerHTML = mealToDisplay.totalFat;
+        document.getElementById('satFat').innerHTML = mealToDisplay.saturatedFat;
+        document.getElementById('transFat').innerHTML = mealToDisplay.transFat;
+        document.getElementById('sodium').innerHTML = mealToDisplay.sodium;
+        document.getElementById('carbs').innerHTML = mealToDisplay.totalCarb;
+        document.getElementById('sugar').innerHTML = mealToDisplay.totalSugar;
+        document.getElementById('fiber').innerHTML = mealToDisplay.fiber;
+        document.getElementById('protein').innerHTML = mealToDisplay.protein;
+        document.getElementById('potassium').innerHTML = mealToDisplay.potassium;
+        document.getElementById('vitA').innerHTML = mealToDisplay.vitaminA;
+        document.getElementById('vitB').innerHTML = mealToDisplay.vitaminB;
+        document.getElementById('vitC').innerHTML = mealToDisplay.vitaminC;
+        document.getElementById('vitD').innerHTML = mealToDisplay.vitaminD;
+        document.getElementById('calcium').innerHTML = mealToDisplay.calcium;
+        document.getElementById('iron').innerHTML = mealToDisplay.iron;
     }
 });
-
-// update rest of the values
