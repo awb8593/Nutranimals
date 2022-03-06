3d557444617588383c17d11f4c5a49ca28bcdcdd
Alex Leute <acleute@gmail.com>
2022-03-06 02:20:48 -0500

    It's done!

--- rng.js
+++ rng.js
@@ -2,29 +2,30 @@
 /** 
  * returns what type of nutranimal gets picked, based on the values that we take in
 */
+"use strict";
 function pickANutranimal(calories, totalFat, saturatedFat, transFat, sodium, totalCarb, fiber, totalSugar, 
     protein, potassium, vitaminA, vitaminB, vitaminC, vitaminD, calcium, iron){ //Yes, this function has 16 arguments. Too bad. (for less jank, make meal.js store these as key-value pair)
-    let valuesArr = // weighting based on daily value that these are more comparable (rounding to an integer)
+    let valuesArr = // weighting based on daily value that these are more comparable https://www.netrition.com/rdi_page.html. The units are different for all of them
     [
-        calories,
-        totalFat,
-        saturatedFat, 
-        transFat,
-        sodium,
-        totalCarb,
-        fiber,
-        totalSugar,
-        protein,
-        potassium,
-        vitaminA,
-        vitaminB,
-        vitaminC,
-        vitaminD,
-        calcium,
-        iron
+        (calories / 2000.0) * 100, //2000 calories daily
+        (totalFat / 78.0) * 100, //78 g
+        (saturatedFat / 20.0) * 100, //20g 
+        (transFat / 2.0) * 100, //you really shouldn't have any of this, so it's weighted really high
+        (sodium / 2300.0) * 100, //2300 mg
+        (totalCarb / 275.0) * 100, //275g
+        (fiber / 28.0) * 100, //28g
+        (totalSugar / 50.0) * 100, //50g
+        (protein / 50.0) * 100, //100g
+        (potassium / 4700.0) * 100, //4700mg
+        (vitaminA / 900.0) * 100, //900mcg
+        (vitaminB / 1.7024) * 100, //1.7 mg and 2.4mcg, lumping together b6 and b12
+        (vitaminC / 90.0) * 100, //90m
+        (vitaminD / 20.0) * 100, //20mcg
+        (calcium / 1300) * 100, //1300mg
+        (iron / 18.0) * 100, //18mg
     ];
     // find the biggest value
-    var biggestValue = 0;
+    var biggestValue = 0.0;
     for (let i = 0; i < valuesArr.length; i++){
         if (valuesArr[i] > biggestValue){
             biggestValue = valuesArr[i];
@@ -66,5 +67,6 @@ function pickANutranimal(calories, totalFat, saturatedFat, transFat, sodium, tot
             return "iron";
     }
 }
-console.log(pickANutranimal(9, 5, 10, 4, 0, 6, 4, 8, 
-    3, 1, 6, 7, 8, 7, 7, 5));
+
+//TODO: less jank
+//TODO: make rng random(when there is more than one animal for nutrition value).
