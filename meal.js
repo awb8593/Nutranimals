51f34e61f5fb5cb2081aa9bc4dfccb98e0298317
awb8593 <awb8593@rit.edu>
2022-03-05 20:10:47 -0500

    meals.js JSON file updating
    successfully updates the meal in the json file (only stores 1 meal currently)

--- meal.js
+++ meal.js
@@ -43,48 +43,50 @@ function jsonReader(filepath, cb) {
 // get the nutritional values
 function getNutrition() {
     var nameField = document.getElementById('meal-name').value;
-    console.log(nameField);
+    console.log("name: ", nameField);
+
+    // make a new meal with the nutritional attributes
+    newMeal = {
+        name: nameField,
+        reflection: null,
+        calories: 500,
+        totalFat: 10,
+        saturatedFat: 10,
+        transFat: 10,
+        sodium: 10,
+        totalCarb: 10,
+        totalSugar: 10,
+        fiber: 10,
+        protein: 10,
+        potassium: 10,
+        vitaminA: 10,
+        vitaminB: 10,
+        vitaminC: 10,
+        vitaminD: 10,
+        calcium: 10,
+        iron: 10
+    }
+
+    save(newMeal);
 }
 
 // save the user's meal into the json file
-function save() {
-    
+function save(newMeal) {
+    // reads from a json file (data = the meal in the list), updates the data, and writes back
+    jsonReader('./meals.json', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            data = newMeal;
+            fs.writeFile('./meals.json', JSON.stringify(data, null, 2), err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
+
 }
 
 var saveButton = document.getElementById('saveButton');
 saveButton.addEventListener('click', getNutrition);
-
-const newMeal = {
-    name: 'New Meal',
-    reflection: null,
-    calories: 500,
-    totalFat: 10,
-    saturatedFat: 10,
-    transFat: 10,
-    sodium: 10,
-    totalCarb: 10,
-    totalSugar: 10,
-    fiber: 10,
-    protein: 10,
-    potassium: 10,
-    vitaminA: 10,
-    vitaminB: 10,
-    vitaminC: 10,
-    vitaminD: 10,
-    calcium: 10,
-    iron: 10
-};
-
-// reads from a json file, updates the data, and writes back
-jsonReader('./meals.json', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        data.calories += 10;
-        fs.writeFile('./meals.json', JSON.stringify(data, null, 2), err => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-});
