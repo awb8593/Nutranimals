905ac65b55221656e95f6d1e777c953908bea89e
Alex Leute <acleute@gmail.com>
2022-03-05 20:15:53 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Made for [Brick Hack 8!](https://brickhack.io)
+Authored by Alex Leute(me!), [Saige Bock](https://github.com/SaigeMoon), and [Adrian Burgos](https://github.com/awb8593)
+# Disclaimer: We are not nutritionists - this app should not be considered medical advice
---
+++ rng.js
@@ -0,0 +1,26 @@
+class RNG{
+    //class to decide what monsters to give based on what your calories are
+    constructor(){
+        this.categories = [
+            "name",
+            reflection,
+            calories,
+            totalFat,
+            saturatedFat,
+            transFat,
+            sodium,
+            totalCarb,
+            totalSugar,
+            fiber,
+            protein,
+            potassium,
+            vitaminA,
+            vitaminB,
+            vitaminC,
+            vitaminD,
+            calcium,
+            iron
+        ]
+    }
+
+}
